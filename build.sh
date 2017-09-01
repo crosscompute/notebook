3df06fe49cda03f9be8a943f25e1e876462eead8
Roy Hyunjin Han <rhh@crosscompute.com>
2017-09-01 19:18:49 +0000

    Update links

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ notebook/js/main.min.js
 terminal/js/main.min.js
 tree/js/main.min.js"
 TEMPORARY_FOLDER=/tmp/notebook
-TARGET_URL=gs://crosscompute-20170501
+TARGET_URL=gs://crosscompute-website-20170828
 
 mkdir -p $TEMPORARY_FOLDER
 python setup.py css js
@@ -14,6 +14,5 @@ cd notebook/static
 for SOURCE_PATH in $SOURCE_PATHS; do
     SCRIPT_NAME=$(dirname $(dirname $SOURCE_PATH))
     mv $SOURCE_PATH $TEMPORARY_FOLDER/${SCRIPT_NAME}.min.js
-    gsutil cp -z js -a public-read $TEMPORARY_FOLDER/$SCRIPT_NAME.min.js $TARGET_URL
+    # gsutil cp -z js -a public-read $TEMPORARY_FOLDER/$SCRIPT_NAME.min.js $TARGET_URL
 done
-
